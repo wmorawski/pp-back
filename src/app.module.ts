c5f6146bf87bc171c255cf9c6d6954c17e999d64
Wiktor Morawski <s430692@instubuntu2018z.vm.wmi.amu.edu.pl>
2019-02-27 16:41:54 +0100

    Initial classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { PartiesController } from './parties/parties.controller';
+import { UsersController } from './users/users.controller';
+import { ChatsController } from './chats/chats.controller';
+import { UsersService } from './users/users.service';
+import { ChatsService } from './chats/chats.service';
+import { PartiesService } from './parties/parties.service';
+import { PartiesModule } from './parties/parties.module';
+import { UsersModule } from './users/users.module';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
     }),
+    PartiesModule,
+    UsersModule,
+    ChatsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PartiesController, UsersController, ChatsController],
+  providers: [AppService, PartiesService, ChatsService, UsersService],
 })
 export class AppModule {}

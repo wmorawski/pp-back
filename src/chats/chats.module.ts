f83e1e1038004339ac22230f329b249ae6becf79
unknown <wm.matuszewski@wp.pl>
2019-04-10 20:51:53 +0200

    added chatsConnection query

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,4 +1,11 @@
+import { ChatsResolver } from './chats.resolver';
+import { PartiesResolver } from './../parties/parties.resolver';
+import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [ChatsResolver],
+  exports: [ChatsResolver],
+})
 export class ChatsModule {}
